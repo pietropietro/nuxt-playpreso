31ae1fdea6f01461e9c0d63ef9251c61d6dda75e
pietro <mini.pietro@associazionerousseau.it>
2021-06-03 14:46:57 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-container>
+        <v-row style="width:100%" justify="end" align="center">
+           <h1>LOGIN</h1>
+        </v-row>
+        <v-row justify="center" no-gutters><v-col>
+            <h1>PLAYPRESO</h1>
+        </v-col></v-row>
+    </v-container>
+</template>
