0a643dca1e831c0015087695690dfceb5d87d9fd
pietro <mini.pietro@associazionerousseau.it>
2021-06-09 11:22:59 +0200

    join euro

--- pages/cup/_cupId/index.vue
+++ pages/cup/_cupId/index.vue
@@ -1,12 +1,52 @@
 <template>
-    <preso-cup-table :id="cupId"/>
+    <div>
+        <!-- <v-text-field v-model="idModel"/> -->
+        <v-row class="pa-4" align="center" v-if="currentUser">
+            <v-btn text @click="joinEuro" :loading="loading" v-if="canJoinEuro">JOIN</v-btn>
+
+            <!-- <v-btn text @click="checkCanJoinEuro" :loading="loading">check</v-btn> -->
+
+        </v-row>
+        <preso-cup-table v-if="!loading" :id="cupId"/>
+    </div>
 </template>
 <script>
 export default {
     data(){
         return{
             cupId: this.$route.params.cupId,
+            canJoinEuro: false,
+            loading: false,
+            idModel: 3
         }
+    },
+    methods:{
+        showerror(){
+            this.$notifier.showError();
+        },
+        async joinEuro(){
+            this.loading = true;
+            let values = [
+                {'action' : "joinPresoCup"},
+                {'userid' : this.idModel},
+                {'cuptypeid' : 5}
+            ]
+            let resp = await this.$api.call(values);
+            this.loading = false;
+            this.checkCanJoinEuro();
+        },
+
+        async checkCanJoinEuro(){
+            let values = [
+                {'action' : "getHomePresoCups"},
+                {'userid' : this.idModel},
+            ]
+            let resp = await this.$api.call(values);
+            this.canJoinEuro = resp.canJoinEuro;
+        }
+    },
+    async mounted(){
+        await this.checkCanJoinEuro();
     }
 }
 </script>
