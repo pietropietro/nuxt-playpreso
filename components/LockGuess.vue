c3b0b15f30b784a30b37567493f430f367af558c
pietro <mini.pietro@associazionerousseau.it>
2021-06-09 22:34:11 +0200

    lockguess component

---
+++ components/LockGuess.vue
@@ -0,0 +1,87 @@
+<template>
+    <v-container fluid>
+        <div style="height:20%">
+            <v-row class="mt-2 mx-2" no-gutters>
+                <v-col>
+                    <h1 class="text-center">{{match.hometeam_name}} - {{match.awayteam_name}}</h1>
+                </v-col>
+            </v-row>
+            <v-row class="my-0" justify="center"><span class="caption">{{match.date_start}}</span></v-row>
+        </div>
+        <v-card style="height:50%">
+            <v-container fill-height>
+                <v-row class="mt-4">
+                    <v-col cols="5">
+                        <h1 class="text-center">{{home + (home === 4 ? '+' : '')}}</h1>
+                        <v-slider
+                            class="mx-4"
+                            :max="4"
+                            :min="0"
+                            v-model="home"
+                        ></v-slider>
+                    </v-col>
+                    <v-col cols="2"><h1 class="text-center">-</h1></v-col>
+                    <v-col cols="5">
+                        <h1 class="text-center">{{away + (away === 4 ? '+' : '')}}</h1>
+                        <v-slider
+                            class="mx-4"
+                            :max="4"
+                            :min="0"
+                            v-model="away"
+                        ></v-slider>
+                    </v-col>
+                </v-row>
+                <v-row justify="center" class="text-center">
+                    <v-col cols="4">
+                        <v-chip color="primary">
+                            <h3>{{computed1x2}}</h3>
+                        </v-chip>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-chip color="primary">
+                            <h3>{{computedUO}}</h3>
+                        </v-chip>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-chip color="primary">
+                            <h3>{{computedGGNG}}</h3>
+                        </v-chip>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
+        <v-row justify="center" class="my-1" style="position:fixed; bottom:70px; width:100%;">
+            <v-btn color="primary" text block><h1>LOCK</h1></v-btn>
+        </v-row>
+    </v-container>
+</template>
+<script>
+export default {
+    name:"LockGuess",
+    props:{
+        guess: {type: Object},
+        match: {type: Object}
+    },
+    data(){
+        return{
+            home: 0,
+            away:0
+        }
+    },
+    computed: {
+        computed1x2(){
+            if(this.home > this.away) return 1;
+            if(this.away > this.home) return 2;
+            return "X";
+        },
+        computedUO(){
+            if(this.home + this.away > 2) return "OVER";
+            return "UNDER";
+        },
+        computedGGNG(){
+            if(this.home >0 && this.away > 0 )return "GOL";
+            return "NOGOL";
+        }
+    }
+}
+</script>
--- pages/cup/_cupId/_groupId/index.vue
+++ pages/cup/_cupId/_groupId/index.vue
@@ -1,22 +1,23 @@
 <template>
     <loading-page v-if="!PCGroup"/>
-    <div v-else>
-        <v-container v-if="selectedPage === 1">
+    <div v-else style="height:100%">
+        <v-container v-if="selectedPage === 1" class="mb-7">
             <v-row justify="center">
                 <h1>{{PCGroup.groupTag}}</h1>
             </v-row>
             <users-standings class="my-5 pa-2" :users="PCGroup.users"/>
             <get-round
-                v-if="currentUser && userInGroup && (!PCGroup.matchBlocks || (PCGroup.matchBlocks[PCGroup.matchBlocks.length - 1].finished && PCGroup.rounds !== PCGroup.matchBlocks.length))"
+                v-if="currentUser && userInGroup && (!PCGroup.matchBlocks || (lastMatchBlock.finished && PCGroup.rounds !== PCGroup.matchBlocks.length))"
                 :level="PCGroup.level" :groupId="PCGroup.id" class="my-5" :refresh="refresh"
             />
             <match-blocks-slider class="pt-5" v-if="PCGroup.matchBlocks"
                 :matchBlocks="PCGroup.matchBlocks" :rounds="PCGroup.rounds" :users="PCGroup.users"
             />
         </v-container>
+        <lock-guess v-else :guess="selectedMBI.guesses[0]" :match="selectedMBI.match" style="height:100%"/>
         <v-pagination
             v-model="selectedPage"
-            :length="!userInGroup || !PCGroup.matchBlocks ? 1 : 1 + PCGroup.matchBlocks[PCGroup.matchBlocks.length - 1].matchBlockItems.length"
+            :length="!userInGroup || !PCGroup.matchBlocks ? 1 : 1 + lastMatchBlock.matchBlockItems.length"
             circle
             class="pagination-fixed pagination-dots"
         />
@@ -37,6 +38,12 @@ export default {
             return this.PCGroup.users.filter(u => {
                 return u.user.user_id === this.currentUser.user_id;
             }).length > 0;
+        },
+        lastMatchBlock(){
+            return this.PCGroup.matchBlocks[this.PCGroup.matchBlocks.length - 1];
+        },
+        selectedMBI(){
+            return this.lastMatchBlock.matchBlockItems[this.selectedPage -2]
         }
     },
     async mounted(){
