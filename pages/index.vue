00217dfb50364fea1f9d9d28f860dedb5b2c3b58
pietro <mini.pietro@associazionerousseau.it>
2021-06-23 17:50:13 +0200

    12

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
             </nuxt-link>
         </v-row>
         <v-row justify="center">
-            <nuxt-link to="/cup/1">
+            <nuxt-link to="/cup/12">
                 <v-img style="max-width:150px;" contain :src="require('@/assets/img/cup/euro.png')"/>
             </nuxt-link>
         </v-row>
