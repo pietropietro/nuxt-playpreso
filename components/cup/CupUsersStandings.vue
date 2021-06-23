8aa39a40e6440c6d1a58f9d019f3c342deeecd10
pietro <mini.pietro@associazionerousseau.it>
2021-06-23 17:49:30 +0200

    cuptotalpoints

--- components/cup/CupUsersStandings.vue
+++ components/cup/CupUsersStandings.vue
@@ -1,12 +1,18 @@
 <template>
     <v-card :flat="flat" :class="flat ? 'white--text' : 'white--text rounded-xl'" >
-        <v-row v-for="i in size" :key="i" justify="space-between"  
+        <v-row no-gutters v-for="i in size" :key="i" justify="space-between" align="center"  
             :class="colorForPosition(i)"
         >
             <template v-if="users && users.length >= i">
-                <h1>{{users[i-1].user.username}}</h1>
-                
-                <h1>{{users[i-1].plPoints}}</h1>
+                <v-col>
+                    <h1>{{users[i-1].user.username}}</h1>
+                </v-col>
+                <v-col class="text-right mr-2" >
+                    <v-row justify="end" align="end">
+                        <h1>{{users[i-1].plPoints}}</h1>
+                        <div v-if="level && level !== 1" class="caption ml-2 mb-2">{{users[i-1].totalPcPoints}}</div>
+                    </v-row>
+                </v-col>
             </template>
             <template v-else><h1>?</h1></template>
         </v-row>
@@ -19,7 +25,8 @@ export default {
         users: {type: Array},
         flat: {type: Boolean},
         pointsToPassThird: {type: Number},
-        size: {type: Number}
+        size: {type: Number},
+        level: {type: Number}
     },
     methods:{
         colorForPosition(i){
