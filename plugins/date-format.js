bde07e4dddd7be1415b34cd4a8dfb919cc03d237
pietro <mini.pietro@associazionerousseau.it>
2021-06-25 11:28:43 +0200

    fix safari date bug

--- plugins/date-format.js
+++ plugins/date-format.js
@@ -3,18 +3,27 @@ import Vue from 'vue'
 Vue.mixin({
     methods:{
         formatDate(dateString){
-            let dateObject = new Date(dateString);
+            //handle SAFARI date error replacing - with /
+            let dateObject = new Date(dateString.replace(/-/g, "/"));
+
+            let hourStyle= { hour: 'numeric', minute: 'numeric'};
+            let dateStyle= {day:'2-digit', month: '2-digit', year: '2-digit', hour: 'numeric', minute: 'numeric'};
+            let weekDateStyle = {weekday:'long', hour: 'numeric', minute: 'numeric'} ;
+            
             if(this.isToday(dateObject)){
-                return 'TODAY ' + Intl.DateTimeFormat('en-GB',{timeStyle: 'short' }).format(dateObject)
+                return 'TODAY ' + Intl.DateTimeFormat('en-GB', hourStyle).format(dateObject);
             }
             if(this.isToday(dateObject, 1)){
-                return 'TOMORROW ' + Intl.DateTimeFormat('en-GB',{timeStyle: 'short' }).format(dateObject)
+                return 'TOMORROW ' + Intl.DateTimeFormat('en-GB', hourStyle).format(dateObject);
             }
             if(this.isToday(dateObject, -1)){
-                return 'YESTERDAY ' + Intl.DateTimeFormat('en-GB',{timeStyle: 'short' }).format(dateObject)
+                return 'YESTERDAY ' + Intl.DateTimeFormat('en-GB', hourStyle).format(dateObject);
+            }
+            if(this.inNextDays(dateObject)){
+                return Intl.DateTimeFormat('en-GB', weekDateStyle).format(dateObject).toString().toUpperCase();
             }
 
-            return Intl.DateTimeFormat('en-GB',{ dateStyle: 'medium', timeStyle: 'short' }).format(dateObject)
+            return Intl.DateTimeFormat('en-GB', dateStyle).format(dateObject);
         },
         isToday(someDate, changeTime) {
             const today = new Date();
@@ -24,6 +33,12 @@ Vue.mixin({
             return someDate.getDate() == today.getDate() &&
                 someDate.getMonth() == today.getMonth() &&
                 someDate.getFullYear() == today.getFullYear()
+        },
+        inNextDays(someDate){
+            let inFiveDays = new Date().setDate( new Date().getDate() + 5);
+            if(someDate < inFiveDays){
+                return true;
+            }
         }
     }
 });
