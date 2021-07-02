a7532765a40461407a2d8aa5e23e51f6e61203bb
pietro <mini.pietro@associazionerousseau.it>
2021-07-02 16:39:43 +0200

    layout

--- components/match/MatchDateScore.vue
+++ components/match/MatchDateScore.vue
@@ -1,9 +1,9 @@
 <template>
     <!-- todo create plugin for matchNotStarted -->
     <span v-if="match.score_home === 222" class="caption">{{formatDate(match.date_start)}}</span>
-    <v-chip v-else label outlined small color="primary">
-        <span class="font-weight-bold caption">{{match.score_home}}-{{match.score_away}}</span>
-    </v-chip>
+    <!-- <v-chip v-else label outlined small color="primary"> -->
+        <h3 v-else class="font-weight-bold">{{match.score_home}}-{{match.score_away}}</h3>
+    <!-- </v-chip> -->
 </template>
 <script>
 export default {
