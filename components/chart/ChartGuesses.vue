c1c6e64822a55cf9325ffdaaab8fe883a1b35a38
pietro <mini.pietro@associazionerousseau.it>
2021-07-09 16:22:19 +0200

    sparkline

--- components/chart/ChartGuesses.vue
+++ components/chart/ChartGuesses.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row>
         <v-col cols="12">
-            <div class="chart" ref="chartGuesses" :style="'height: 40vh'"/>
+            <div class="chart" ref="chartGuesses" :style="'height: 10vh'"/>
         </v-col>
     </v-row>
 </template>
@@ -20,16 +20,18 @@ export default {
         }
     },
     mounted(){
-        this.elaboratedData = this.guesses.map(g => {
+        this.elaboratedData = this.guesses.map((g,i) => {
             return {
                 score: g.preso_score,
-                match: g.match.hometeam_name + "-" + g.match.awayteam_name
+                match: g.match.hometeam_name + "-" + g.match.awayteam_name,
+                opacity: i === 0 ? 1 : 0
             }
         }).reverse();
         this.createChart();
     },
     methods:{
         createChart(){
+            let chartColor = "blue";
             let chart = am4core.create(this.$refs.chartGuesses, am4charts.XYChart);
             chart.data = this.elaboratedData;
 
@@ -44,57 +46,32 @@ export default {
             let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
             categoryAxis.renderer.grid.template.strokeOpacity = 0;
             valueAxis.renderer.grid.template.strokeOpacity = 0;
+            valueAxis.renderer.baseGrid.disabled = true;
             valueAxis.min = 0;
             valueAxis.max = 19;
+            
+            // valueAxis.startLocation = 0.5;
+            // valueAxis.endLocation = 0.7;
+            valueAxis.renderer.labels.template.disabled = true;
+            categoryAxis.renderer.labels.template.disabled = true;
 
-
-            valueAxis.renderer.labels.template.adapter.add("text", function(text, target) {
-                // if(["2","4","5","7","9","19"].includes(text)){
-                //     return text;
-                // }
-                return "";
-            });
-            categoryAxis.renderer.labels.template.adapter.add("text", function(text, target) {
-                return "";
-            });
-
-
-            // var dropShadow = new am4core.DropShadowFilter();
-            // dropShadow.dy = 1;
-            // dropShadow.dx = 1;
-            // dropShadow.opacity = 0.5;
-            // valueAxis.tooltip.filters.push(dropShadow);
-            // valueAxis.tooltip.background.strokeWidth = 0;
-            // valueAxis.tooltip.background.cornerRadius = 3;
-            // valueAxis.tooltip.background.pointerLength = 0;
-            // valueAxis.tooltip.background.fill = "blue"
-            // valueAxis.adapter.add("getTooltipText", function(text, target) {
-            //     // let m = moment(text).locale('it-IT').format('D MMMM Y')
-            //     return "[bold white] "+ text + " [/]";
-            // });
+            
 
 
             let series = chart.series.push(new am4charts.LineSeries());
             series.dataFields.valueY = "score";
             series.dataFields.categoryX = "match";
-            series.tensionX = 0.92;
-            series.strokeWidth = 6;
-            series.stroke = "blue";
+            series.tensionX = 0.9;
             
-            var circleBullet = series.bullets.push(new am4charts.CircleBullet());
-            // circleBullet.circle.strokeWidth = 4;
-            // circleBullet.circle.radius = 6;
-            // circleBullet.circle.fill = "white";
-            // circleBullet.circle.strokw = "blue";
-            // circleBullet.adapter.add("fill", function(fill, target){
-            //     if(target.dataItem.dates.dateX){
-            //         let add = target.dataItem.dates.dateX.getMonth() % 2;
-            //         return COLOR_GRADIENT[index + add];
-            //     }
-            // });  
+            series.strokeWidth = 4;
+            series.stroke = chartColor;
+            var bullet = series.bullets.push(new am4charts.CircleBullet());
+            bullet.circle.opacity = 0;
+            bullet.circle.fill = chartColor;
+            bullet.circle.propertyFields.opacity = "opacity";
+            bullet.circle.radius = 3;
             series.minBulletDistance = 20;
-            series.fill = "blue";
-            // series.fillOpacity = 0.2;
+            series.fill = chartColor;
         },
     }
 }
