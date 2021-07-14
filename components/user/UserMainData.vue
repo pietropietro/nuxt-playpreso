df893f262b84d8fa95801599aa0bda52fe2bf73b
pietro <mini.pietro@associazionerousseau.it>
2021-07-14 12:41:41 +0200

    profile wip

--- components/user/UserMainData.vue
+++ components/user/UserMainData.vue
@@ -1,51 +1,36 @@
 <template>
-        <v-container fluid class="ocrastd text-uppercase">
-            <v-row no-gutters justify="center" align="center">
-                <v-col class="text-center" cols="4">
-                    <h1 style="font-size: 10vw;">{{average}}</h1>
-                </v-col>
-                <v-col>
-                    <h1 style="font-size:8vw;">{{username}}</h1>
-                </v-col>
-            </v-row>
-            <v-row no-gutters>
-                <v-col cols="4" class="text-center" style="font-size: 4vw;">LAST 3<br> MONTHS</v-col>
-                <v-col style="font-size: 7vw;">{{guessesVerified.length}} MATCHES</v-col>
-            </v-row>
-            <v-row>
-                <v-col v-if="lastPreso">
-                    <v-row justify="center"> 
-                        {{lastPreso.match.hometeam_name.substring(0,3)}} - {{lastPreso.match.awayteam_name.substring(0,3)}}
-                    </v-row>
-                    <v-row justify="center">
-                        {{lastPreso.match.score_home}} - {{lastPreso.match.score_away}}
-                    </v-row>
-                </v-col>
-                <v-col>
-                    <v-row>
-                        <v-col>
-                            <div class="caption">1X2</div>
-                            <h1>{{unox2}}%</h1>
-                        </v-col>
-                        <v-col>
-                            <div class="caption">UO2.5</div>
-                            <h1>{{uo25}}%</h1>
-                        </v-col>
-                        <v-col>
-                            <div class="caption">GGNG</div>
-                            <h1>{{ggng}}%</h1>
-                        </v-col>    
-                        <v-col>
-                            <div class="caption">PRESO</div>
-                            <h1>{{presoCount}}</h1>
-                        </v-col>
-                    </v-row>
-                </v-col>
-            </v-row>
-        </v-container>
-    <!-- <v-row class="text-center">
-        
-    </v-row> -->
+    <v-container class="ocrastd text-uppercase text-center">
+        <v-row no-gutters justify="center" align="center">
+            <v-col>
+                <h1 style="font-size: 4em;">{{Math.round(average * 100) / 100}}</h1>
+            </v-col>
+            <v-col>
+                <h1>{{username}}</h1>
+            </v-col>
+        </v-row>
+        <v-row align="center">
+            <v-col><span>LAST 3<br> MONTHS</span></v-col>
+            <v-col><span>{{guessesVerified.length}} <br> MATCHES</span></v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="6" md="3">
+                <div class="caption">1X2</div>
+                <h1>{{Math.round(unox2 * 100) / 100}}%</h1>
+            </v-col>
+            <v-col cols="6" md="3">
+                <div class="caption">UO2.5</div>
+                <h1>{{Math.round(uo25 * 100) / 100}}%</h1>
+            </v-col>
+            <v-col cols="6" md="3">
+                <div class="caption">GGNG</div>
+                <h1>{{Math.round(ggng* 100) / 100}}%</h1>
+            </v-col>    
+            <v-col cols="6" md="3">
+                <div class="caption">PRESO</div>
+                <h1>{{presoCount}}</h1>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
 export default {
@@ -57,15 +42,12 @@ export default {
     data(){
         return{
             guessesVerified : [],
-            lastPreso: null,
             presoCount: 0,
         }
     },
     mounted(){
         this.guessesVerified = this.guesses.filter(g => g.verified);
-        this.lastPreso = this.guesses.filter(g => g.PRESO);
-        this.presoCount = this.lastPreso.length;
-        this.lastPreso = this.presoCount > 0 ? this.lastPreso[0] : null ;
+        this.presoCount = this.guesses.filter(g => g.PRESO).length;
     },
     computed:{
         average(){
