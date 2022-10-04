6792e88fe697d802cad645b775e869db28e05439
pietro <pietro@playpreso.com>
2022-10-04 11:41:25 +0200

    rounds pagination ui update

--- components/PPRound/Pagination.vue
+++ components/PPRound/Pagination.vue
@@ -10,22 +10,6 @@
                 <div class=" text-center" v-else>{{i}}</div>
             </v-col>
         </v-row>
-        <!-- <v-row align="center"> -->
-            <!-- <v-col cols="auto">
-                <p-p-simple-pagination
-                    :length="ppRounds.length"
-                    :value="selectedRound"
-                    :setValue="(newVal) => selectedRound = newVal"
-                >
-                    <v-row justify="center" align="end" slot="selected-page">
-                        <p-p-info :value="selectedRound" :value2="rounds"/>
-                    </v-row>
-                </p-p-simple-pagination>
-            </v-col> -->
-            <!-- <v-col v-for="(item, index) in ppRounds[selectedRound - 1].best" :key="index" cols="6" md="3">
-                <p-p-info :label="'#' + (index+1) " :value="item.username" :value2="'+'+item.sum_points" small/>
-            </v-col> -->
-        <!-- </v-row> -->
         <p-p-round-display
             class="ma-0"
             v-for="(ppRM,i) in ppRounds[selectedRound - 1].ppRoundMatches" 
