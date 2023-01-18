c66921bcf6302977aca65daf49afbc4abb059bb5
pietro <pietro@playpreso.com>
2023-01-18 01:27:43 +0100

    admin pick match various

--- components/admin/match/Picker.vue
+++ components/admin/match/Picker.vue
@@ -7,30 +7,52 @@
         <loading-page v-if="loading"/>
         <template v-else>
             <v-row>
-                <v-col v-for="league in leagues" :key="league.id">{{league.name}}</v-col>
+                <v-col v-for="league in leagues" :key="league.id">
+                    <league-detail :league="league" class="text-h3"></league-detail>
+                </v-col>
             </v-row>
-            <v-expansion-panels class="py-4">
-                <v-row v-if="picked.length > 0">
-                    <v-col v-for="match in picked" :key="match.id" >
-                        <admin-match-expansion-panel :match="match"/>
-                    </v-col>
+            <v-container>
+                <v-row>
+                    <h1>PICKED</h1>
                 </v-row>
-                <v-row v-else-if="called" justify="center" class="py-5">
-                    <b>no matches found</b>
+                <v-row>
+                    <v-expansion-panels class="py-4">
+                        <v-row v-if="picked.length > 0">
+                            <v-col v-for="match in picked" :key="match.id" >
+                                <admin-match-expansion-panel :match="match"/>
+                            </v-col>
+                        </v-row>
+                        <v-row v-else-if="called" justify="center" class="py-5">
+                            <b>no matches found</b>
+                        </v-row>
+                    </v-expansion-panels>
                 </v-row>
-            </v-expansion-panels>
+                <v-row>
+                    <h1>ALL ({{all.length}})</h1>
+                </v-row>
+                <v-row>
+                    <v-expansion-panels class="py-4">
+                        <v-row v-if="all.length > 0">
+                            <v-col v-for="match in all" :key="match.id" >
+                                <admin-match-expansion-panel :match="match"/>
+                            </v-col>
+                        </v-row>
+                    </v-expansion-panels>
+                </v-row>
+            </v-container>
         </template>
     </v-container>
 </template>
 <script>
 export default {
-    data:()=>({ppTTselected:null, loading:false, picked:[], leagues: [], called: false}),
+    data:()=>({ppTTselected:null, loading:false, picked:[], leagues: [], all:[], called: false}),
     methods:{
         async testPick(){
             this.loading=true;
             let response = await this.$api.call(this.ADMIN_API_ROUTES.MATCH.PICK + this.ppTTselected);
             if(response && response.status === "success"){
-                this.picked = response.message.matches;
+                this.picked = response.message.picked_matches;
+                this.all = response.message.all_matches;
                 this.leagues = response.message.leagues;
             }
             this.called=true;
