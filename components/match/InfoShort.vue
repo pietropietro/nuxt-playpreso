9f58a822e1e8e332506784e6868e9d3a73d3c56f
pietro <pietro@playpreso.com>
2023-03-23 16:47:51 +0100

    team names long fix on match info

--- components/match/InfoShort.vue
+++ components/match/InfoShort.vue
@@ -1,12 +1,10 @@
 <template>
     <v-container class="pa-0 lh-1">
         <league-detail :league="match.league" />
-        <v-row class="flex-nowrap">
-            <v-col cols="auto">
-                <h4 v-if="match.homeTeam?.name.length < 19">{{match.homeTeam?.name}}</h4>
-                <h6 v-else>{{match.homeTeam?.name}}</h6>
-                <h4 v-if="match.awayTeam?.name.length < 19">{{match.awayTeam?.name}}</h4>
-                <h6 v-else>{{match.awayTeam?.name}}</h6>
+        <v-row >
+            <v-col>
+                <div  v-html="teamNameToHtml(this.match.homeTeam?.name)" />
+                <div  v-html="teamNameToHtml(this.match.awayTeam?.name)" />
             </v-col>
             <v-col cols="auto" class="pl-0" v-if="match.verified_at" >
                 <h4>{{match.score_home}}</h4>
@@ -24,6 +22,33 @@ export default {
         match: {type: Object, required: true},
         showDateTime: {type:Boolean, default: true},
         withTime: {type: Boolean, default: true}
+    },
+    methods:{
+        teamNameToHtml(name) {
+            console.log(this.$vuetify.breakpoint.mdAndUp);
+            if(this.$vuetify.breakpoint.mdAndUp) return name;
+            
+            let limitOneWord = 18;
+            if (name.length <= limitOneWord) {
+                return `<h4>${name}</h4>`;
+            }
+
+            let moreWordsLimit = 9;
+            const words = name.split(' ');
+            let h4Text = '';
+            let h6Text = '';
+
+            for (let i = 0; i < words.length; i++) {
+                const word = words[i];
+                if (h4Text.length + word.length > moreWordsLimit || h6Text.length) {
+                    h6Text += ` ${word}`;
+                } else {
+                    h4Text += ` ${word}`;
+                }
+            }
+
+            return `<h4 style="display:inline">${h4Text.trim()} <h6 style="display:inline">${h6Text.trim()}</h6></h4>`;
+        },
     }
 }
 </script>
