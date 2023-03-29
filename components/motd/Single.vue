79952abc6b1293212fda741b32b0a7a1f802f894
pietro <pietro@playpreso.com>
2023-03-29 19:09:17 +0200

    display motd stats

--- components/motd/Single.vue
+++ components/motd/Single.vue
@@ -1,20 +1,38 @@
 <template>
-    <v-row justify="center" align="center">
-        <v-col>
-            <div style="min-width:250px; max-width:300px;">
-                <guess-single-card
-                    :match="motd.match"
-                    :guess="motd.guess"
-                    :afterLock="afterLock"
-                    :rgb="motdPPTT.rgb"
-                />
-            </div>
-        </v-col>
-        <v-col class="text-center">
-            <h1>{{motd.aggr_count}}</h1>
-            <div class="overline lh-1">locks</div>
-        </v-col>
-    </v-row>
+    <div>
+        <v-row justify="center" align="center">
+            <v-col>
+                <div style="min-width:250px; max-width:300px;">
+                    <guess-single-card
+                        :match="motd.match"
+                        :guess="motd.guess"
+                        :afterLock="afterLock"
+                        :rgb="motdPPTT.rgb"
+                    />
+                </div>
+            </v-col>
+            <v-col class="text-center">
+                <h1>{{motd.aggr_count}}</h1>
+                <div class="overline lh-1">locks</div>
+            </v-col>
+        </v-row>
+        <v-row v-if="motd.stats" class="text-center">
+            <v-col cols="4">
+                <h3>{{motd.stats.most_locked.result}}</h3>
+                <div class="overline lh-1">most <em-emoji id="lock"/></div>
+            </v-col>
+            <v-col cols="4">
+                <h3>{{motd.stats.preso_count}}</h3>
+                <div class="overline lh-1">PRESO</div>
+
+            </v-col>
+            <v-col cols="4">
+                <h3>{{motd.stats.points_avg}}</h3>
+                <div class="overline lh-1">AVG</div>
+
+            </v-col>
+        </v-row>
+    </div>
 </template>
 <script>
 export default {
