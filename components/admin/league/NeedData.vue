37fbfdc6f7d0a6e3cf02a4da1f735aef8aa66584
pietro <pietro@playpreso.com>
2023-08-04 12:44:50 +0200

    admin getneeddata improvement

--- components/admin/league/NeedData.vue
+++ components/admin/league/NeedData.vue
@@ -1,13 +1,24 @@
 <template>
-    <v-container>
-        <league-detail v-for="league in leagues" :key="league.id" :league="league"/>
+    <loading-page v-if="loading" />
+    <v-container v-else>
+        <v-row justify="center" no-gutters>
+            <v-switch inset 
+                v-model="withGuessesModel" 
+                label="w/guess"
+                @change="getNeedData()"
+                :loading="loading"
+            />
+        </v-row>
+        <admin-league-table :leagues="leagues" :refresh="getNeedData"/>
     </v-container>
 </template>
 <script>
 export default {
     data(){
         return{
-            leagues: null
+            leagues: null,
+            loading: null,
+            withGuessesModel: false
         }
     },
     async mounted(){
@@ -15,7 +26,10 @@ export default {
     },
     methods:{
         async getNeedData(){
-            let response = await this.$api.call(this.ADMIN_API_ROUTES.LEAGUE.GET_NEED_DATA);
+            this.loading= true;
+            let response = await this.$api.call(this.ADMIN_API_ROUTES.LEAGUE.GET_NEED_DATA + 
+                (this.withGuessesModel ? '?withGuesses=1' : '')
+            );
             if(response && response.status === "success"){
                 this.leagues = response.message;
             }
