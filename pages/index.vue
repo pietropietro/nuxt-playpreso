b15addff943abf4c7f54f872d3c085b88798aa5b
pietro <pietro@playpreso.com>
2024-01-14 14:18:41 +0100

    layout mobile only index

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,22 @@
 <template>
     <v-container  class="pt-0" >
         <guess-home-scroll />        
-        <v-row v-if="!$vuetify.breakpoint.mdAndUp">
+        <v-row>
             <v-col cols="12" >
                 <p-p-counter />
             </v-col>
-            <v-col class="pt-0 pt-md-3" cols="12" >
-                <motd-home class="mt-n6 mt-md-5"/>
+            <v-col class="pt-0" cols="12" >
+                <motd-home class="mt-n6"/>
             </v-col>
         </v-row>
         <v-row>
             <v-col cols="12" 
                 class="pa-0"
             >
-                <p-p-counter v-if="$vuetify.breakpoint.mdAndUp"/>
                 <p-p-league-available-list class="px-2 mb-5"/>
                 <user-participation-enrolled-list />
             </v-col>
             <v-col cols="12">
-                <motd-home v-if="$vuetify.breakpoint.mdAndUp" class="mt-n6 mt-md-5"/>
                 <stats-top-users class="mt-5"/>
                 <stats-last-preso class="mt-8 pb-5"/>
             </v-col>
