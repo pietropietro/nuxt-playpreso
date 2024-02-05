8d60baf8ebefe761740b590e2fb82a3070d05a60
pietro <pietro@playpreso.com>
2024-02-05 11:49:42 +0100

    guess alarm wip

--- components/guess/MatchIntroBox.vue
+++ components/guess/MatchIntroBox.vue
@@ -5,19 +5,19 @@
             @click="onClick ? onClick(guess) : null"
             :style="{
                 height: cardHeight,
+                minWidth: cardHeight,
                 overflow: 'hidden'
             }"
         >   
             <!--  ALARM IF LESS THAN 1 day not motd -->
-            <!-- <guess-single-alarm-col
-                :big="big"
-                :guess="guess" 
-                :match="match"
-                :rgb="rgb"
-                :height="cardHeight"
-            /> -->
-            <!-- END ALARM -->
-            <v-col>
+            <v-col  v-if="isWithinNext24Hours && !guess.guessed_at">
+                <guess-single-alarm-col
+                    :rgb="rgb"
+                    :height="cardHeight"
+                />
+            </v-col>
+
+            <v-col v-else>
                 <match-graphic-preview 
                     :rgb="rgb" 
                     :match="match" 
@@ -42,6 +42,22 @@ export default {
             shades:{
                 unlocked:  this.ppRGBA(this.rgb, 0.6) 
             },
+        }
+    },
+    computed:{
+        isWithinNext24Hours() {
+            if(!this.guess || !this.match)return false;
+            // Get the current timestamp
+            let currentTime = new Date().getTime();
+
+            // Calculate the timestamp for the current time plus 24 hours
+            let next24Hours = currentTime + (24 * 60 * 60 * 1000);
+
+            // Parse the given match's start date
+            let matchDate = new Date(this.match?.date_start).getTime();
+
+            // Compare the match's start date with the current time plus 24 hours
+            return matchDate <= next24Hours;
         }
     },
 }
