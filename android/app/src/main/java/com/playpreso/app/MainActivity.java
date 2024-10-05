3983f62928751f229b82ea2fe33618843cb158df
pietro <pietro@playpreso.com>
2024-10-05 17:51:57 +0200

    push notifications android

--- android/app/src/main/java/com/playpreso/app/MainActivity.java
+++ android/app/src/main/java/com/playpreso/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.playpreso.app;
 
+import android.os.Bundle;
+import android.util.Log;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		try {
+			// Check if FirebaseApp is already initialized, avoid reinitializing
+			if (FirebaseApp.getApps(this).isEmpty()) {
+				FirebaseApp.initializeApp(this);  // Initialize Firebase
+				Log.i("FirebaseInit", "Firebase initialized successfully.");
+			}
+		} catch (Exception e) {
+			Log.e("FirebaseInitError", "Error initializing Firebase: " + e.getMessage());
+		}
+	}
+}
