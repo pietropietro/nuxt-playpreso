aea9ef318b70133d3c5af9ae6954372acb9c17e7
pietro <pietro@playpreso.com>
2024-10-08 16:47:19 +0200

    user notification unlocked_guess_starting

--- pages/notification.vue
+++ pages/notification.vue
@@ -5,17 +5,22 @@
             <v-row v-for="(notification,i) in userNotifications" :key="notification.id"
                 justify="center" 
                 :style=" i % 2==0 ? {backgroundColor: 'var(--v-primary-darken3)'} 
-                : {}"
+                : {}" 
             >
-               <template v-if="notification.event_type == 'guess_verified'">
+               <template v-if="['guess_unlocked_starting', 'guess_verified'].includes(notification.event_type)">
+                    <v-col cols="12" class="pt-1 pb-0 mb-n1">
+                        <span class="overline lh-1">
+                            {{notification.event_type == 'guess_unlocked_starting' ? 'lock reminder' : 'match finished'}}
+                        </span>
+                    </v-col>
                     <v-col cols="auto">
                         <guess-box-view
                             :guess="notification.guess"
                             :match="notification.guess.match"
                             :rgb="notification.guess.ppTournamentType.rgb"
                             :afterLock="null"
-                            :onUnlockedClick="null"
-                            :open="true"
+                            :onUnlockedClick="clickUnlocked"
+                            :open="(notification.guess.guessed_at || notification.guess.verified_at) ?  true : false"
                             :setOpen="()=>null"
                         />
                     </v-col>
@@ -58,11 +63,28 @@ export default {
             this.loading=true;
             let response = await this.$api.call(this.API_ROUTES.USER_NOTIFICATION.GET_UNREAD + '?enriched=1', null, 'GET');
             if(response && response.status === "success"){
-                if(!response.message) this.$router.push('/');
                 this.userNotifications = response.message;
+                if(this.userNotifications.length == 0){
+                    this.$router.push('/');
+                }
             }
             this.loading = false;
         },
+        async clickUnlocked(guess){
+            await this.triggerHapticFeedback();
+            
+            let list = [];
+            this.userNotifications.map((un)=>{
+                if(un.guess && !un.guess.guessed_at && !un.guess.verified_at){
+                    list.push(un.guess);
+                }
+            }
+            );
+            this.$store.dispatch('openGuesses/update', {
+                newGuess: guess,
+                newList:  list
+            });
+        }
     },
     async mounted(){
         this.$store.dispatch(
