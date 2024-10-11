d808eaf2bca1c381b1ab2e3398daf84221a3d943
pietro <pietro@playpreso.com>
2024-10-11 17:00:46 +0200

    terms and conditions

--- components/PP/menu/Brain.vue
+++ components/PP/menu/Brain.vue
@@ -1,56 +1,75 @@
 <template>
-    <v-container fill-height  class="pa-5">
-        <v-app-bar 
-            color="var(--v-background-base)"
-            flat 
-            app
-            scroll-threshold="150"
-            ref="appBar"
-            class="pp-app-bar safe-area"
-        >
-            <transition name="fade">
-                <v-row justify="center" class="ocrastd">
-                    <v-col cols="2"> </v-col>
-                    <v-col class="text-center">
-                        <h1>MENU</h1>
-                    </v-col>
-                    <v-col cols="2">
-                        <h1 @click="$store.dispatch('menu/updateMenu', { newVal: false });">
-                            X
-                        </h1>
-                    </v-col>
-                </v-row>
-            </transition>
-        </v-app-bar>
-        <v-container>
-            <v-row justify="center" 
-                v-for="(m,i) in computedMenus" :key="i" 
-                v-html="m.title" 
-                @click="()=>selectedMenu=selectedMenu ? null : m.key" 
-            />
-            <v-row justify="center" v-if="selectedMenu==='points'">
-                <table>
-                    <tr v-for="(item,i) in pointsList" :key="i">
-                        <td class="ocrastd">{{item.text}}</td>
-                        <td class="px-5 caption">{{item.description}}</td>
-                        <td class="overline lh-1">{{item.points}}</td>
-                    </tr>
-                </table>
-            </v-row>
-            <v-row v-if="selectedMenu==='contact'" justify="center" class="overline lh-1">
-                pietro(at)playpreso.com
-            </v-row>
-            <p-p-menu-three-plus v-if="selectedMenu==='3+'" />
-            <p-p-menu-email-preferences v-if="selectedMenu==='email_reminders'" />
-            <p-p-menu-notification-preferences v-if="selectedMenu==='notification_settings'" />
-        </v-container>
-        <v-container style='align-self:end'>
+    <v-container  
+        style="display: flex; flex-direction:column;  height:100%; overflow-y: hidden;" 
+        class="safe-area pa-5"
+    >
+        <v-row justify="center" class="ocrastd" style="flex-grow: 0;">
+            <v-col cols="2"> </v-col>
+            <v-col class="text-center">
+                <h1>MENU</h1>
+            </v-col>
+            <v-col cols="2">
+                <h1 @click="$store.dispatch('menu/updateMenu', { newVal: false });">
+                    X
+                </h1>
+            </v-col>
+        </v-row>
+        <div 
+            style="flex-grow: 1; display: flex; flex-direction: column;"
+        >       
+        <v-spacer/>
+                <div
+                    v-for="(m,i) in computedMenus" :key="i"
+                    class="overline py-2"
+                    style="flex-grow:0"
+                    @click="()=>selectedMenu=selectedMenu ? null : m.key"
+                >
+                    {{selectedMenu == m.key ? 'x ' : ''}}<h3>{{ m.title }}</h3>
+                </div>
+
+                <div class="px-5">
+                    <v-row justify="center" v-if="selectedMenu==='points'">
+                        <table>
+                            <tr v-for="(item,i) in pointsList" :key="i">
+                                <td class="ocrastd">{{item.text}}</td>
+                                <td class="px-5 caption">{{item.description}}</td>
+                                <td class="overline lh-1">{{item.points}}</td>
+                            </tr>
+                        </table>
+                    </v-row>
+                    <v-row v-if="selectedMenu==='contact'" justify="center" class="overline lh-1 my-4">
+                        pietro(at)playpreso.com
+                    </v-row>
+                    <p-p-menu-three-plus v-if="selectedMenu==='3+'" />
+                    <p-p-menu-email-preferences v-if="selectedMenu==='email_reminders'" />
+                    <p-p-menu-notification-preferences v-if="selectedMenu==='notification_settings'" />
+                    <div v-if="selectedMenu==='terms'"
+                        class="caption lh-1 pb-5"
+                        style="white-space: pre-wrap; "
+                    >
+                        <div >{{ termsContent }}</div>
+                    </div>
+                </div>
+
+        <v-spacer/>
+    </div>
+
+
+        <div style="flex-grow: 0;">
             <v-row justify="center" @click="doLogout">
                 <h1 class="ocrastd">LOGOUT</h1>
             </v-row>
             <!-- <v-row justify="center" class="overline lh-1">barona, milano</v-row> -->
             <v-row justify="center"><div class="overline lh-1">v{{VERSION}}</div></v-row>
-        </v-container>
+            <!-- <v-row class="caption" justify="center">
+                <label>
+                    <span @click="()=>selectedMenu='terms'">
+                        Terms and Conditions
+                    </span>
+                </label>
+            </v-row> -->
+        </div>
+
     </v-container>
 </template>
 <script>
@@ -66,11 +85,12 @@ export default {
             selectedMenu: null,
             menus:[
                 // {title: '<h3 class="ocrastd">THEME</h3>', key:'theme'},
-                {title: '<h3 class="ocrastd">NOTIFICATIONS 🔔</h3>', key:'notification_settings'},
-                {title: '<h3 class="ocrastd">EMAIL 🔔</h3>', key:'email_reminders'},
-                {title: '<h3 class="ocrastd">3+</h3>', key:'3+'},
-                {title: '<h3 class="ocrastd">POINTS</h3><em-emoji id="parking"/>', key:'points'},
-                {title: '<h3 class="ocrastd">CONTACT</h3>', key:'contact'},
+                {title: 'NOTIFICATIONS 🔔', key:'notification_settings'},
+                {title: 'EMAIL 🔔', key:'email_reminders'},
+                {title: '3+', key:'3+'},
+                {title: 'POINTS 🅿️', key:'points'},
+                {title: 'CONTACT', key:'contact'},
+                {title: 'TERMS', key:'terms'},
             ],
             pointsList:[
                 {
@@ -97,12 +117,22 @@ export default {
         }
     },
     methods:{
+        data:()=>({termsContent:null}),
         doLogout(){
             this.$logout.logout();
             this.$store.dispatch('menu/updateMenu', { newVal: false });
-        }
+        },
+        async loadTerms() {
+			try {
+				const response = await fetch('/terms.txt'); // Fetches the terms.txt file from static
+				this.termsContent = await response.text(); // Stores the content in termsContent
+			} catch (error) {
+				console.error('Error loading terms:', error);
+				this.termsContent = 'Could not load terms at this time.';
+			}
+		}
     }, 
-    mounted(){
+    async mounted(){
         this.$store.dispatch(
             'navigation/updateTitle', 
             {
@@ -111,6 +141,7 @@ export default {
                 newOverline: null
             }        
         );
+        await this.loadTerms();
     }
 }
 </script>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,6 +1,17 @@
-<template>	
+<template>
 	<guest-screen v-if="!currentUser">
-		<template slot="content">
+	  <template slot="content" v-if="termsFlag">
+		  <!-- Close button to hide the terms -->
+		  <v-row no-gutters justify="end" class="ocrastd mt-n4">
+			<div @click="()=>termsFlag=false"><h2>X</h2></div>
+		  </v-row>
+  
+		  <!-- Scrollable terms section -->
+		  <div class="caption lh-1" style="max-height: 350px; overflow-y: auto;">
+			<div class="terms-text">{{ termsContent }}</div>
+		  </div>
+		</template>
+		<template slot="content" v-else>
 			<div class="ocrastd">
 				<v-row no-gutters>
 					<v-col cols="12">
@@ -34,6 +45,18 @@
 					<h1>SIGN UP</h1>
 				</v-btn>
 			</v-row>
+			<v-row class="pb-4 mx-4">
+				<div class="caption lh-1">
+					<label>
+						By signing up, you agree to the
+						<span @click="()=>termsFlag=true" 
+							style="text-decoration: underline;"	
+						>
+							Terms and Conditions
+						</span>
+					</label>
+				</div>
+			</v-row>
 			<v-row justify="space-between">
 				<nuxt-link :to="ROUTES.LOGIN" class="no-decoration">
 					<v-btn block text>
@@ -57,7 +80,9 @@ export default {
 			username: null,
 			password: null,
 			loading: false,
-			emailAdd: null
+			emailAdd: null,
+			termsFlag: false,
+			termsContent: null
 		}
 	},
 	methods:{
@@ -70,7 +95,21 @@ export default {
 			if(e.keyCode === 32){
 				e.preventDefault(); return;
 			}
+		},
+		async loadTerms() {
+			try {
+				const response = await fetch('/terms.txt'); // Fetches the terms.txt file from static
+				this.termsContent = await response.text(); // Stores the content in termsContent
+			} catch (error) {
+				console.error('Error loading terms:', error);
+				this.termsContent = 'Could not load terms at this time.';
+			}
 		}
+	},
+	async mounted() {
+		console.log('mounted signup');
+		await this.loadTerms();
 	},
 }
 </script>
+
