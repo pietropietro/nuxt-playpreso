539b00ab457d38e7e5084403ecdb8b5ae43dc17e
pietro <pietro@playpreso.com>
2024-10-15 13:53:22 +0200

    dummy privacy page (ios requested it)

---
+++ pages/privacy.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+        <h1 class="ocrastd">PRIVACY POLICY</h1>
+      <div style="white-space: pre-wrap;">
+          Introduction
+          At PlayPreso, we value your privacy and are committed to protecting your personal information. This Privacy Policy explains how we collect, use, and safeguard your information when you use our app.
+      
+          Information We Collect
+          When you sign up for PlayPreso, we collect your username, email address, and password. Your password is stored in a hashed format and cannot be viewed by us. We may also collect information regarding your interactions with the app, such as your score predictions and tournament activity.
+      
+          How We Use Your Information
+          We use the information collected to provide, maintain, and improve the PlayPreso app. Specifically, we use your email address to send notifications about tournament updates or important account-related information. We may also use your data to analyze app usage trends and improve user experience.
+      
+          Third-Party Services
+          We use third-party services such as Firebase for push notifications. These services may collect certain information about your device and interactions with the app to deliver these notifications effectively.
+      
+          Cookies
+          We may use cookies to enhance your experience in the app, such as to store preferences or session data.
+      
+          Data Security
+          We implement appropriate technical and organizational measures to protect your information from unauthorized access, disclosure, or alteration.
+      
+          Changes to This Privacy Policy
+          We may update this Privacy Policy from time to time. Any changes will be posted on this page, and you are advised to review this policy periodically for updates.
+      
+          Contact Us
+          If you have any questions about this Privacy Policy, please contact us at:
+          Email: pietro@playpreso.com
+      </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
