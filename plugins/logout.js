0eb038da405f668d6bae5c17fc766e1c94746ec8
pietro <pietro@playpreso.com>
2024-11-19 11:41:04 +0100

    logout plugin simpler

--- plugins/logout.js
+++ plugins/logout.js
@@ -3,10 +3,7 @@ export default ({app, store},inject) => {
         async logout(){
             if(store.state.user.currentUser == null && store.state.user.token == null)  return;
             await app.$pushNotificationsPlugin.removeNotificationTokenFromServer();
-            store.commit('user/updateNotificationToken', { notificationToken: null});
-            store.commit('user/updateCurrentUser', { currentUser: null});
-            store.commit('user/updateToken', { token: null});
-            store.commit('user/updatePoints', { points: null});
+            store.commit('user/clearUser');
             app.router.push('/');
         }
     })
