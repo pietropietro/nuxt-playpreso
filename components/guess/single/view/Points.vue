a6e898310b36fb8ea7ba7efa479d8222778b81d1
pietro <pietro@playpreso.com>
2025-01-23 12:22:45 +0100

    points toggle with guess_win_prize

--- components/guess/single/view/Points.vue
+++ components/guess/single/view/Points.vue
@@ -4,13 +4,20 @@
             v-if="guess.verified_at"
             justify="center" align="center"
         >
-            <em-emoji v-if="isMissed(guess) && (!guess.id || ['motd','flash'].includes(guess.id))" id="dash" size="2em"/>
-            <em-emoji v-else-if="isMissed(guess)" id="x" size="2em"/>
+            <template v-if="toggleState">
+                <div class="overline lh-1">
+                    win<br>{{guess.winner_prize}}
+                </div>
+            </template>
             <template v-else>
-                <p-p-preso v-if="guess.PRESO"/>
-                <h2 v-else>
-                    {{guess.points}}
-                </h2>
+                <em-emoji v-if="isMissed(guess) && (!guess.id || ['motd','flash'].includes(guess.id))" id="dash" size="2em"/>
+                <em-emoji v-else-if="isMissed(guess)" id="x" size="2em"/>
+                <template v-else>
+                    <p-p-preso v-if="guess.PRESO"/>
+                    <h2 v-else>
+                        {{guess.points}}
+                    </h2>
+                </template>
             </template>
         </v-row>
     </div>
@@ -19,6 +26,26 @@
 export default {
     props:{
         guess: {type:Object}
+    },
+    data() {
+        return {
+            toggleState: false,    // toggles true/false every second
+            intervalId: null,
+        };
+    },
+    mounted() {
+        // If match is live, start toggling
+        if (this.guess.winner === 1 && this.guess.ppTournamentType.name=='Flash') {
+            this.intervalId = setInterval(() => {
+                this.toggleState = !this.toggleState;
+            }, 2000);
+        }
+    },
+    beforeDestroy() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
     },
 }
 </script>
